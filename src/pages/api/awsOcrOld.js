76b2fa2793ea9e8bf1d1a7be1cfd31abc525bb53
Justin Lee <justinlee38@outlook.com>
2023-09-29 00:06:55 -0700

    ocr working

--- src/pages/api/awsOcrOld.js
+++ src/pages/api/awsOcrOld.js
@@ -152,6 +152,7 @@
 // }
 
 const AWS = require("aws-sdk");
+const OpenAI = require("openai");
 
 export default async function handler(req, res) {
   // Configure AWS SDK
@@ -178,13 +179,16 @@ export default async function handler(req, res) {
 
   const checkVideoTextDetectionStatus = async (jobId) => {
     let response;
+    console.log("in check video text detection");
     do {
       response = await rekognition.getTextDetection({ JobId: jobId }).promise();
+      console.log(response);
+
       if (response.JobStatus === "SUCCEEDED") {
         console.log("SUCCEEDED");
         var keywords = new Set();
         response.TextDetections.forEach((item) => {
-          console.log(typeof item);
+          //   console.log(typeof item);
           if (
             String(item).includes("TikTok") ||
             String(item).includes("@bucket")
@@ -218,6 +222,55 @@ export default async function handler(req, res) {
     // Check video text detection status and retrieve detected texts
     const detectedTexts = await checkVideoTextDetectionStatus(jobId);
     console.log(detectedTexts);
+
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "Your job is to clean data to get locations from objects.",
+        },
+        {
+          role: "user",
+          content:
+            "Given set of strings, get all the specific locations\n\nSet: {\n  'TikTok',\n  '@bucketlistbums',\n  '10 Most Instagrammable',\n  'Places in Istanbul',\n  '10',\n  'Most',\n  'Instagrammable',\n  'Places',\n  'in',\n  'Istanbul',\n  'ل TikTok',\n  'ل',\n  'Suleymaniye Mosque',\n  'Suleymaniye',\n  'Mosque',\n  'Galata Tower',\n  'VITAVIEN',\n  'KARVE',\n  'Galata',\n  'Tower',\n  'E',\n  'LIMAN',\n  'Ajwa Hotel Steps',\n  'Ajwa',\n  'Hotel',\n  'Steps',\n  '30',\n  'Blue Mosque',\n  'Blue',\n  'Balat',\n  'Neighborhood',\n  'Balat Neighborhood',\n  'Seven Hills Rooftop Terrace',\n  'S',\n  'Seven',\n  'Hills',\n  'Rooftop',\n  'Terrace',\n  'DE',\n  'Basilica Cistern',\n  'Basilica',\n  'Cistern',\n  'Topkapi Palace',\n  'Topkapi',\n  'Palace',\n  'Taht Rooftop',\n  'Taht',\n  'Grand Bazaar',\n  'Grand',\n  'Bazaar',\n  'Fed',\n  'FedEx',\n  'Follow me on Instagram',\n  'AJWA',\n  'Follow',\n  'me',\n  'on',\n  'Instagram',\n  'AIWA'\n}",
+        },
+        {
+          role: "assistant",
+          content:
+            "Cleaned Set: {\n  'Suleymaniye Mosque',\n  'Galata Tower',\n  'Ajwa Hotel Steps',\n  'Blue Mosque',\n  'Balat Neighborhood',\n  'Seven Hills Rooftop Terrace',\n  'Basilica Cistern',\n  'Topkapi Palace',\n  'Taht Rooftop',\n  'Grand Bazaar'\n}",
+        },
+        {
+          role: "user",
+          content: `Given set of strings, get all the specific locations ${JSON.stringify(
+            [...detectedTexts]
+          )}`,
+        },
+      ],
+      temperature: 1,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+    console.log(response);
+
+    console.log(response.choices[0]);
+
+    console.log(response.choices[0].message);
+
+    return response;
+    // const param = {
+    //   method: "POST",
+    //   body: { data: detectedTexts },
+    // };
+    // const locationResult = await handler(param, res);
+    console.log(locationResult);
+
     // detectedTexts.forEach((item) => {
     //   console.log(item ? item : null);
     // });
--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -5,10 +5,22 @@ import { makePostRequest } from "../../lib/api";
 const Test = () => {
   const fetchSerp = async () => {
     try {
-      const serpResult = await makePostRequest(
+      console.log("start ocr");
+
+      const ocrResult = await makePostRequest(
         `${process.env.URL}/api/awsOcrOld`
       );
-      return serpResult.result;
+      console.log(ocrResult);
+      // console.log("completed ocr");
+
+      // const openAIResult = await await makePostRequest(
+      //   `${process.env.URL}/api/openai_ocr_location`,
+      //   ocrResult
+      // );
+      // console.log("completed openai");
+
+      console.log(openAIResult.choices[0].message.content);
+      return openAIResult.choices[0].message.content;
     } catch (error) {
       console.error(error);
     }
