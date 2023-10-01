92d18bb6f0a29aae3fd30c4f8b1ed486fb0c2498
edwardtanoto <ed27tanoto@gmail.com>
2023-10-02 03:05:14 +0800

    try change viewport

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="theme-color" content="#000000" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
         />
 
         <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
